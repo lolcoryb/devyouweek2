ab0cc48ae66aa5731d8c27337d8a63c9ea7dd882
lolcoryb <71185428+lolcoryb@users.noreply.github.com>
2021-04-19 21:35:37 -0400

    Add files via upload
    Class Lab Calculator

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Class_Lab_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calculator excercise
+
+            Console.WriteLine("Welcome to C# Calculator.\r\n \r\nPlease enter the first number of your calculation\r\n");
+
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Now choose the second number.\r\n");
+
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {a + b}\r\n");
+            Console.WriteLine($"{a} - {b} = {a - b}\r\n");
+            Console.WriteLine($"{a} / {b} = {a / b}\r\n");
+            Console.WriteLine($"{a} * {b} = {a * b}\r\n");
+
+            Console.ReadLine();
+        }
+    }
+}
